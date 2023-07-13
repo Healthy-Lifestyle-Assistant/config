3c45597c7579c57669065c7793fa4a302cea3553
Oleg Kuchrenko <OlegKDeveloper@gmail.com>
2023-07-13 15:48:12 +0300

    Update nginx.sh

--- script/nginx.sh
+++ script/nginx.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 git clone https://github.com/Healthy-Lifestyle-Assistant/client.git
 
-cp -a ~/client/dist /var/www/html
-# cp -a ~/client/dist /usr/share/nginx/html
+sudo cp -a ~/client/dist /var/www/html
+# sudo cp -a ~/client/dist /usr/share/nginx/html
 
-rm -rf ~/client
+sed -i 's/domain_or_IP/replacement_text' ~/config/nginx/nginx.config
 
-cp ~/config/nginx.config /etc/nginx/sites-available/
-ln -s /etc/nginx/sites-available/nginx.config /etc/nginx/sites-enabled/
+sudo cp ~/config/nginx.config /etc/nginx/sites-available/
 
-systemclt restart nginx
+sudo ln -s /etc/nginx/sites-available/nginx.config /etc/nginx/sites-enabled/
+
+sudo nginx -t
+
+sudo systemclt restart nginx
