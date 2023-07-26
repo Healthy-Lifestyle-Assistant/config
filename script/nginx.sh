ef4811363099f024333c9073430befd729f5fe23
OlegKDev <OlegKDeveloper@gmail.com>
2023-07-26 11:33:58 +0300

    Update Nginx files

--- script/nginx.sh
+++ script/nginx.sh
@@ -6,7 +6,7 @@ sudo cp -a ~/client/dist /var/www/html
 
 sed -i 's/domain_or_IP/replacement_text' ~/config/nginx/nginx.config
 
-sudo cp ~/config/script/nginx.config /etc/nginx/sites-available/
+sudo cp ~/config/nginx/nginx.config /etc/nginx/sites-available/
 
 sudo ln -s /etc/nginx/sites-available/nginx.config /etc/nginx/sites-enabled/
 
